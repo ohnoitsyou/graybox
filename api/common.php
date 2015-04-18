35525f143ad0970d4c6828cc295ab27f9d4c93a0
David Young <ohnoitsyou7043@gmail.com>
2015-04-18 03:46:33 -0400

    Adding common functions

---
+++ api/common.php
@@ -0,0 +1,51 @@
+<?php 
+$dbhostname = 'studentdb.gl.umbc.edu';
+$dbusername = 'dayoung1';
+$dbpassword = 'dayoung1';
+$dbdatabase = 'dayoung1';
+$con;
+$db;
+function dbLogin() {
+  global $dbhostname, $dbusername, $dbpassword, $con;
+  $con = mysql_connect($dbhostname , $dbusername, $dbpassword);
+  return $con;
+}
+
+function dbSelect() {
+  global $dbdatabase, $db;
+  $db = mysql_select_db($dbdatabase);
+  return $db;
+}
+
+function executeQuery($query) {
+  global $con;
+  global $db;
+  if(!$con) { die("You must login first"); }
+  if(!$db)  { die("You must select the database first"); }
+  $results = mysql_query($query);
+  $resultsArray = array();
+  if(mysql_num_rows($results) > 0) {
+    for($i = 0; $i < mysql_num_rows($results); $i++) {
+      $resultsArray[] =  mysql_fetch_assoc($results);
+    }
+  }
+  return $resultsArray;
+}
+
+function getLocations($zipcode) {
+  $query = "";
+  if($zipcode = "00000" || $zipcode = "") {
+    $query = "select * from locations;";
+  } else {
+    $query = sprintf("select * from locations where zip = '%s';",
+              mysql_real_escape_string($zipcode));
+  }
+  return executeQuery($query);
+}
+
+function getUserInfo($userid) {
+  $query = sprintf("select * from users where userID = '%s';",
+            mysql_real_escape_string($userid));
+  return executeQuery($query);
+}
+?>
