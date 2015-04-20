177a9edcf74988f0d877c11c88d43956a84855d2
David Young <ohnoitsyou7043@gmail.com>
2015-04-19 20:44:19 -0400

    Merging Christians changes

--- connectivity.php
+++ connectivity.php
@@ -1,3 +1,8 @@
+<?php
+session_start();  
+include('api/common.php');
+?>
+
 <!DOCTYPE html PUBLIC "-//w3c//DTD XHTML 1.1//EN"
   "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 
@@ -8,11 +13,9 @@
 
   </head>
 <body>
+
 <?php
-session_start();  
-include('api/common.php');
-
-
+    
 $username = $_POST['username'];
 $password = $_POST['password'];
 
@@ -37,7 +40,7 @@ $results=mysql_query($sql));
 //Mysql_num_row is counting table row
 $count=mysql_num_rows($result);
 
-//If result matched $myusername and $mypassword, table row must be 1 row
+//If result matched $username and $password, table row must be 1 row
 if($count==1){
 
 //Register $username, $password and redirect to file "login_success.php"
