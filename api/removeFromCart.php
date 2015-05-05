f35aeffc9996ceb4d1c68740215e25584182dfeb
David Young <ohnoitsyou7043@gmail.com>
2015-05-05 03:49:56 -0400

    New API functions!

---
+++ api/removeFromCart.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+$itemID = $_POST['itemID'];
+$foundMatch = false;
+for($i = 0; $i < sizeof($_SESSION['cart']); $i++) {
+  $item = $_SESSION['cart'][$i];
+  if($item == $itemID) {
+    unset($_SESSION['cart'][$i]);
+    $foundMatch = true;
+  }
+}
+$_SESSION['cart'] = array_values($_SESSION['cart']);
+?>
