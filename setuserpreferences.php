2573491f7216a80c0ffd80e11706d8dacca014b2
David Young <ohnoitsyou7043@gmail.com>
2015-05-05 13:46:35 -0400

    Christians changes

--- setuserpreferences.php
+++ setuserpreferences.php
@@ -1,18 +1,20 @@
+<?php
+session_start();
+include('api/common.php');
+?>
+
 <!DOCTYPE html PUBLIC "-//w3c//DTD XHTML 1.1//EN"
   "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 
 <html xmlns = "http://www.w3.org/1999/xhtml">
   <head>
-    <title>Login</title>
+    <title>Set user preferences</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 
   </head>
 <body>
 
 <?php
-session_start();
-include('api/common.php');
-
 $action = $_POST["action"];
 $children = $_POST["children"];
 $comedies = $_POST["comedies"];
@@ -23,19 +25,14 @@ $horror = $_POST["horror"];
 $sci_fi = $_POST["sci_fi"];
 $tv = $_POST["tv"];
 $thrillers = $_POST["thrillers"];
-
 echo "Your preferences are saved! <br/>";
 echo "They have been recorded.";
-
 //Connect to database server
 dbLogin();
-
 //Select database
 dbSelect();
-
 //execute the query to insert the above PHP variables into the preferences table
 $sql = executeQuery("INSERT INTO preferences (pref1,pref2,pref3,pref4,pref5,pref6,pref7,pref8,pref9,pref10) VALUES ('$action','$children','$comedies','$documentaries','$dramas','$foreign','$horror','$sci_fi','$tv','$thrillers');");
-
 $result = mysql_query($query);
 if(! $result){
 	print("Error - query could not be executed");
