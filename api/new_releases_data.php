44c86aa83fca9e21d818cf1feadc029ef7f4c9c4
semigon <koj1@umbc.edu>
2015-05-05 16:36:57 -0400

    Changed new_release_data to point to right page

--- api/new_releases_data.php
+++ api/new_releases_data.php
@@ -1,5 +1,5 @@
 <?php
-require_once ('api/common.php');
+require_once ('common.php');
 dbLogin();
 dbSelect();
 $movie_query = "SELECT DISTINCT inventoryID from inventory where releaseDate between DATE_SUB(Now(), INTERVAL 30 DAY) AND NOW() ORDER BY inventoryID DESC LIMIT 5;";
