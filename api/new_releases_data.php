2c90b4d49e456ee282c60873c7b7617e53faef14
David Young <ohnoitsyou7043@gmail.com>
2015-05-05 17:53:53 -0400

    MERGE jon remote to local

--- api/new_releases_data.php
+++ api/new_releases_data.php
@@ -4,6 +4,6 @@ dbLogin();
 dbSelect();
 $movie_query = "SELECT DISTINCT * from inventory where releaseDate > NOW() ORDER BY inventoryID DESC LIMIT 1;";
 $results = executeQuery($movie_query);
-echo "<img src=\"img/<?php echo $movie['inventoryID']; ?>.jpg\" alt=\"<?php echo $movie['iName']; ?>\" height=\"300px\" width=\"250px\" onmouseover=\"this.style.height='500px';this.style.width='300px';\" onmouseout=\"this.style.height='300px';this.style.width='250px';\" />";
+echo "<img src=\"img/" . $results[0]['inventoryID'] . ".jpg\" alt=\"" . $results[0]['iName'] . "\" height=\"300px\" width=\"250px\" onmouseover=\"this.style.height='500px';this.style.width='300px';\" onmouseout=\"this.style.height='300px';this.style.width='250px';\" />";
 ?>
 
