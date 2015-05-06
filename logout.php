6434fecc1eb16628b3ea7c5338171e3e8f5c3688
cnieva <christian.a.nieva@gmail.com>
2015-05-06 15:36:34 -0400

    Layout update

--- logout.php
+++ logout.php
@@ -1,27 +1,33 @@
+<?php
+session_start();
+session_destroy();
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8"> 
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="css/style.css">
-  <!--
-   <link rel="stylesheet/less" type="tet/css" href="css/style.less">
-  <script src="js/less.js" type="text/javascript"></script>
-  -->
 </head>
 <body>
   <div class="header">
     <div class="navbar">
       <span class="logo">Graybox powered by RentalVideo&copy;</span>
       <div class="quick_links">
-        <span><a href="login.html">Home</a></span><span><a href="new_releases.php">New Releases</a></span><span><a href="displayall.php">Available titles to rent</a></span>
+        <span><a href="login.html">Home</a></span>
       </div>
     </div>
   </div>
   <div class="content">
-    <div class="title"><h1><!-- add a title here --><h1></div>
-    <div class="_page"> <!-- change to <your_page_name>_page -->
-    <!-- Insert your content here -->
+    <div class="title"><h1>Logout Success!<h1></div>
+    <div class="_page">
+
+            <a href="http://userpages.umbc.edu/~dayoung1/is448/graybox/">
+                <img src="http://sheriff.oktibbeha.ms.us/images/Back_home_button.png" alt="Go back home">
+            </a>
+
     </div>
   </div>
   <div class="footer">
