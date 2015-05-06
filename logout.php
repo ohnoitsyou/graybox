6b3cf6a7a12ab2c661cab905cdbbc6bffef11935
David Young <ohnoitsyou7043@gmail.com>
2015-05-06 15:28:37 -0400

    importing Christians logout page

--- logout.php
+++ logout.php
@@ -2,3 +2,33 @@
 session_start();
 session_destroy();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <title>Logout Success!</title>
+</head>
+
+<body>
+  <div class="header">
+    <div class="navbar">
+      <span class="logo">Graybox powered by RentalVideo&copy;</span>
+      <div class="quick_links">
+        <span><a href="login.html">Home</a></span>
+  <div class="content">
+    <div class="title"><h1>Logout Success!<h1></div>
+
+            <a href="http://userpages.umbc.edu/~dayoung1/is448/graybox/">
+                <img src="http://sheriff.oktibbeha.ms.us/images/Back_home_button.png" alt="Go back home">
+            </a>
+
+
+  <div class="footer">
+    <span>&copy; 2015 Team Zero Two Point Oh</span>
+  </div>
+         <?php functionFooter(); ?>
+</body>
+</html>
