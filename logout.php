180ab880ba7371fdcf6f69e80a04cf0ba5ce9edd
David Young <ohnoitsyou7043@gmail.com>
2015-05-06 15:29:47 -0400

    Resetting christians logout page

--- logout.php
+++ logout.php
@@ -1,31 +1,35 @@
-<?php 
-session_start();
-session_destroy();
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
+  <meta charset="utf-8"> 
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="css/style.css">
-  <title>Logout Success!</title>
+  <!--
+   <link rel="stylesheet/less" type="tet/css" href="css/style.less">
+  <script src="js/less.js" type="text/javascript"></script>
+  -->
 </head>
-
 <body>
   <div class="header">
     <div class="navbar">
       <span class="logo">Graybox powered by RentalVideo&copy;</span>
       <div class="quick_links">
-        <span><a href="login.html">Home</a></span>
+        <span><a href="login.html">Home</a></span><span><a href="new_releases.php">New Releases</a></span><span><a href="displayall.php">Available titles to rent</a></span>
+      </div>
+    </div>
+    <div class="statusbar">
+      <div class="statusboxes">
+          <?php displayUserbox(); ?>
+        <?php displayCartbox(); ?>
+      </div>
+    </div>
+  </div>
   <div class="content">
-    <div class="title"><h1>Logout Success!<h1></div>
-
-            <a href="http://userpages.umbc.edu/~dayoung1/is448/graybox/">
-                <img src="http://sheriff.oktibbeha.ms.us/images/Back_home_button.png" alt="Go back home">
-            </a>
-
-
+    <div class="title"><h1><!-- add a title here --><h1></div>
+    <div class="_page"> <!-- change to <your_page_name>_page -->
+    <!-- Insert your content here -->
+    </div>
+  </div>
   <div class="footer">
     <span>&copy; 2015 Team Zero Two Point Oh</span>
   </div>
